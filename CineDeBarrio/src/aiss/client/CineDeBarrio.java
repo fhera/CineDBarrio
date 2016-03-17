5d2897bf2143e262994cdef629cc98248c72ff52
fernando <fheraa@gmail.com>
2016-03-17 20:42:10 +0100

    #Página web-> Acerca de nosotros (Terminada)

--- CineDeBarrio/src/aiss/client/CineDeBarrio.java
+++ CineDeBarrio/src/aiss/client/CineDeBarrio.java
@@ -1,37 +1,11 @@
 package aiss.client;
 
-import java.util.HashMap;
-
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.HTMLPanel;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Panel;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class CineDeBarrio implements EntryPoint {
 
-	HorizontalPanel contenidoSelector = new HorizontalPanel();
-
+	
 	public void onModuleLoad() {
-		init();
-		go("init", new HashMap<String, String>());
-
-	}
-
-	private void go(String token, HashMap<String, String> params) {
-		Panel p = RootPanel.get();
-		if (token == "list" || token == "init") {
-			p.clear();
-			p.add(new ViewList(params));
-		} else if (token == "create") {
-			p.add(new ViewCreate(params));
-		}
-	}
-
-	public void init() {
-		final VerticalPanel mainPanel = new VerticalPanel();
-		
 		
 	}
 }

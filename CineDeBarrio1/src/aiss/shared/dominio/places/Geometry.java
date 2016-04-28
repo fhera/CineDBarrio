d339729aa73de96d8b47ebdecc468d71d9a4f2d8
PepeRodriAdams <ponieb@hotmail.com>
2016-04-28 19:32:55 +0200

    #Agregamos google places

---
+++ CineDeBarrio1/src/aiss/shared/dominio/places/Geometry.java
@@ -0,0 +1,29 @@
+
+package aiss.shared.dominio.places;
+
+import javax.annotation.Generated;
+
+@Generated("org.jsonschema2pojo")
+public class Geometry {
+
+    private Location location;
+
+    /**
+     * 
+     * @return
+     *     The location
+     */
+    public Location getLocation() {
+        return location;
+    }
+
+    /**
+     * 
+     * @param location
+     *     The location
+     */
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+}
